c9ab4d1763be0f11fff26963127c8d1df0b669f9
wuxingogo <52111314ly@gmail.com>
2016-06-07 14:54:27 +0800

    init

---
+++ CecilTest/CecilTest/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CecilTest
+{
+    class Program
+    {
+        static void Main( string[] args )
+        {
+        }
+    }
+}
