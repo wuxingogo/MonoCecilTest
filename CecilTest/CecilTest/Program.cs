f5b26cf7e34ee45ab08077db23dcba489e30c53c
wuxingogo <52111314ly@gmail.com>
2016-06-07 15:44:01 +0800

    change method call replace string

--- CecilTest/CecilTest/Program.cs
+++ CecilTest/CecilTest/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Mono.Cecil;
+using Mono.Cecil.Cil;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +12,58 @@ namespace CecilTest
     {
         static void Main( string[] args )
         {
+            ModuleDefinition module = ModuleDefinition.ReadModule( "WuxingogoEditor.dll" );
+            var assembiy = module.Assembly;
+            foreach( var item in module.Types )
+            {
+                if( item.Name.Equals( "XQucickSetPrefs" ) )
+                {
+                    Console.WriteLine( "Seach XQuickSetPrefs" );
+                    var definition = item;
+                    foreach( var method in definition.Methods )
+                    {
+                        if( method.Name.Equals( "OnXGUI" ) )
+                        {
+                            foreach( Instruction inst in method.Body.Instructions )
+                            {
+                                if( inst.OpCode.Name == "ldstr" && ( ( string )inst.Operand ).Contains( "Value" )) //如果发现操作数包含, Value! ===>改为"SB Value!";
+                                {
+                                    inst.Operand = "Hello Value";
+                                }
+
+                            }
+                                Console.WriteLine( "Seach OnXGUI" );
+                            var ins = method.Body.Instructions[0];
+                            var worker = method.Body.GetILProcessor();
+                            worker.InsertBefore( ins, worker.Create( OpCodes.Ldstr, "Method start…" ) );
+                            worker.InsertBefore( ins, worker.Create( OpCodes.Call,
+                                assembiy.MainModule.Import( typeof( Console ).GetMethod( "WriteLine", new Type[] { typeof( string ) } ) ) ) );
+                            ins = method.Body.Instructions[method.Body.Instructions.Count - 1];
+
+                            worker.InsertBefore( ins, worker.Create( OpCodes.Ldstr, "Method finish…" ) );
+                            worker.InsertBefore( ins, worker.Create( OpCodes.Call,
+                                assembiy.MainModule.Import( typeof( Console ).GetMethod( "WriteLine", new Type[] { typeof( string ) } ) ) ) );
+                            module.Write( "IL.dll" );
+                            break;
+                        }
+                    }
+                    //AssemblyFactory.SaveAssembly( assembiy, "IL_" + args[0] );
+                
+                    
+                }
+            
+        }
+            Console.Read();
+
+
+            //foreach( TypeDefinition type in module.Types )
+            //{
+            //    if( !type.IsPublic )
+            //        continue;
+
+            //    Console.WriteLine( type.FullName );
+            //}
+            //Console.Read();
         }
     }
 }
